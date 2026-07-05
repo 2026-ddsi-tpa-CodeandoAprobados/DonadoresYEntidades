d649f67f6516e7e1506f9a561cf4badcb3923f40
JorgeIgnacioCondoriAguirre <jcondoriaguirre@frba.utn.edu.ar>
2026-07-05 00:29:35 -0300

    try cath a donacionClient

--- src/main/java/ar/edu/utn/dds/k3003/service/Fachada.java
+++ src/main/java/ar/edu/utn/dds/k3003/service/Fachada.java
@@ -96,7 +96,11 @@ public class Fachada implements FachadaDonadoresYEntidades {
             throw new RuntimeException("La queja ya existe");
         }
         buscarDonadorPorID(quejaDTO.donadorID());
-        donacionClient.getDonacion(quejaDTO.donacionID());
+        try{
+            donacionClient.getDonacion(quejaDTO.donacionID());
+        } catch (RuntimeException e) {
+            throw new RuntimeException("No existe una donacion con ese ID");
+        }
         donacionClient.postQueja(quejaDTO.donacionID());
 
         val queja = quejaDataMapper.toQueja(quejaDTO);
